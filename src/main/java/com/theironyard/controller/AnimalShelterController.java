6eddf8bced23dd402bdadffe94aff8b92b884338
Melanie Johnson <meljohn23@msn.com>
2016-10-04 18:05:03 -0400

    tues 10-4 push to master

--- src/main/java/com/theironyard/controller/AnimalShelterController.java
+++ src/main/java/com/theironyard/controller/AnimalShelterController.java
@@ -2,6 +2,7 @@ package com.theironyard.controller;
 
 import com.theironyard.entity.Animal;
 import com.theironyard.entity.AnimalType;
+import com.theironyard.entity.Search;
 import com.theironyard.repository.AnimalRepo;
 import com.theironyard.repository.NoteRepo;
 import com.theironyard.repository.TypeRepo;
@@ -24,72 +25,80 @@ import java.util.List;
 //@RequestMapping("/")
 public class AnimalShelterController {
 
-    @Autowired
-    // we are NOT creating an instance of the AnimalRepo interface - just grabbing an instance thanks to Spring
+    @Autowired // we are NOT creating an instance of the AnimalRepo interface - just grabbing an instance thanks to Spring
     private AnimalRepo animalRepo;
 
     @Autowired
     private TypeRepo typeRepo;
 
     @Autowired
-    private AnimalService animalService;
+    private NoteRepo noteRepo;
 
     @Autowired
-    private NoteRepo noteRepo;
+    private AnimalService animalService;
 
     // path refers to the URL path | method refers to HTML method
     @RequestMapping(path = "/", method = RequestMethod.GET)
-    public String showAnimals(Model model) { // this method "showAnimals()" responds to HTTP GET requests
-
-        // String text = animalService.getAnimal(8);
+    public String showAnimals(Model model, Search search, String action){ // this method "showAnimals()" responds to HTTP GET requests
 
         List<Animal> allAnimals = animalRepo.findAll();
         List<AnimalType> types = typeRepo.findAll();
-        model.addAttribute("types", types);
         // put the animal into the model
         model.addAttribute("animals", allAnimals);
+        model.addAttribute("types", types);
         //NOTE: the Model class is part of Spring MVC that holds your model data and passes that to your view
         // this is similar to how we can set attributes on an HttpServletRequest in Servlets
 
         return "HomePg"; // this links to an html file in resources -> templates (named HomePg)
     }
 
-    @RequestMapping(path = "/HomePg", method = RequestMethod.GET)
-    public String showAnimal(Model model) { // this method "showAnimals()" responds to HTTP GET requests
-        // String text = animalService.getAnimal(8);
 
-//        Integer count = noteRepo.findAll().size();
-//        List<Note> notes = noteRepo.findAll();
+//    @RequestMapping(path = "/HomePg", method = RequestMethod.GET)
+//    public String showAnimal(Model model){ // this method "showAnimals()" responds to HTTP GET requests
+//        // String text = animalService.getAnimal(8);
 //
-//        model.addAttribute("count", count);
-        List<Animal> allAnimals = animalRepo.findAll();
-        List<AnimalType> types = typeRepo.findAll();
-        model.addAttribute("types", types);
-        // put the animal into the model
-        model.addAttribute("animals", allAnimals);
-        //NOTE: the Model class is part of Spring MVC that holds your model data and passes that to your view
-        // this is similar to how we can set attributes on an HttpServletRequest in Servlets
+//        List<Animal> allAnimals = animalRepo.findAll();
+//        List<AnimalType> types = typeRepo.findAll();
+//        //List<Note> notes = noteRepo.findAll();
+//
+//
+//        // put the animal into the model
+//        model.addAttribute("animals", allAnimals);
+//        model.addAttribute("types", types);
+//        //NOTE: the Model class is part of Spring MVC that holds your model data and passes that to your view
+//        // this is similar to how we can set attributes on an HttpServletRequest in Servlets
+//
+//        return "HomePg"; // this links to an html file in resources -> templates (named HomePg)
+//    }
 
-        return "HomePg"; // this links to an html file in resources -> templates (named HomePg)
-    }
+//    @RequestMapping(path = "Home", method = RequestMethod.POST)
+//    public String showList(Model model, String name, String typeId){
+//
+//        List<Animal> animals = animalRepo.findByNameAndType(name, typeId);
+//        // put the animal into the model
+//        model.addAttribute("animals", animals);
+//
+//        //NOTE: the Model class is part of Spring MVC that holds your model data and passes that to your view
+//        // this is similar to how we can set attributes on an HttpServletRequest in Servlets
+//
+//        return "HomePg"; // this links to an html file in resources -> templates (named HomePg)
+//    }
 
-    @RequestMapping(path = "/AddOrEdit", method = RequestMethod.GET)
+
+    @RequestMapping (path = "/AddOrEdit", method = RequestMethod.GET)
     public String addEditAnimal(Model model) {
         return "AddOrEdit";
     }
 
-    @RequestMapping(path ="/DeleteAnimal", method = RequestMethod.GET)
+    @RequestMapping (path = "/DeleteAnimal", method = RequestMethod.GET)
     public String deleteAnimal(Model model) {
         return "redirect:/";
-    }
+    } // front slash is web root
 
-    @RequestMapping(path = "/Notes", method = RequestMethod.GET)
-    public String showNotes(Model model, Long id) {
-
+    @RequestMapping (path = "/Notes", method = RequestMethod.GET)
+    public String notes(Model model, Long id) {
         Animal animal = animalRepo.findOne(id);
         model.addAttribute("animal", animal);
-
-
         return "NotePg";
-    }
+    } // front slash is web root
 }
---
+++ src/main/java/com/theironyard/entity/Search.java
@@ -0,0 +1,28 @@
+package com.theironyard.entity;
+
+/**
+ * Created by katherine_celeste on 10/4/16.
+ */
+public class Search {
+    Integer typeId;
+    String name;
+
+    public Search() {
+    }
+
+    public Integer getTypeId() {
+        return typeId;
+    }
+
+    public void setTypeId(Integer typeId) {
+        this.typeId = typeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
