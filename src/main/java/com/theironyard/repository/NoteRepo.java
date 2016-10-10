0d0fd7115ec6e3130ebad0e5fd037f6d4f1677ff
Katherine Guzman <kceleste35@gmail.com>
2016-10-10 10:55:13 -0400

    deleted target dir

--- src/main/java/com/theironyard/repository/NoteRepo.java
+++ src/main/java/com/theironyard/repository/NoteRepo.java
@@ -1,9 +1,10 @@
 package com.theironyard.repository;
 
-import com.sun.tools.javac.util.List;
 import com.theironyard.entity.Note;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * Created by katherine_celeste on 9/19/16.
